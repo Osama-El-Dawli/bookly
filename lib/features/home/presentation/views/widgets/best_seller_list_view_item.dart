3197839979c4a82ab43d9b5b9cafd9da9b7267ec
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-08-04 04:39:46 +0300

    ``` Refactor best seller list view item and update linter rules
    - Updated the best seller list view item to include book rating and price, enhancing the UI with more detailed book information.
    - Corrected the font family in the text style to ensure consistent styling across the app.
    - Introduced a new BookRating widget to encapsulate rating display logic, promoting reusability and cleaner code.
    - Upgraded flutter_lints dependency from version 3.0.2 to 4.0.0 to adhere to the latest linting standards and improve code quality.
    - Adjusted pubspec.lock to reflect the updated dependency version.
    
    This commit aims to improve the user interface and maintainability of the codebase by incorporating more detailed book information and adhering to updated linting standards.
    ```

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -28,16 +29,39 @@ class BestSellerListViewItem extends StatelessWidget {
             width: 30,
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
-                child: const Text(
+                child: Text(
                   'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(
+                    fontFamily: 'GT Sectra Fine',
+                  ),
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const BookRating(),
+                ],
+              )
             ],
           )
         ],
