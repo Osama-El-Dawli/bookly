c91b2beaf622a2458c663786cd97038af977d79e
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-08-08 16:46:19 +0300

    Add similar books section and refine book details layout
    - Introduced a new `SimilarBooksListView` widget to display a horizontal list of similar books at the bottom of the book details page.
    - Adjusted the layout of the `BookDetailsViewBody` to include a title for the similar books section and properly space the new section.
    - Fixed the alignment and padding of the `BoxAction` widget to ensure consistent spacing and alignment within the layout.
    - Modified the aspect ratio of the `CustomBookImage` widget to slightly adjust the image display size for better visual consistency.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.
 import 'package:bookly_app/features/home/presentation/views/widgets/box_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -50,10 +51,21 @@ class BookDetailsViewBody extends StatelessWidget {
           const SizedBox(
             height: 37,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: BoxAction(),
+          const BoxAction(),
+          const SizedBox(
+            height: 50,
           ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksListView(),
         ],
       ),
     );
