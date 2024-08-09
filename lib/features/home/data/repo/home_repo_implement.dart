b55b3b7c72ced8f8efb592d82e28056d0d605e67
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-08-09 17:47:31 +0300

    Add API Service and Implement Home Repository for Fetching Newest Books
    - Introduced `ApiService` class to handle API requests using Dio package.
    - Implemented `HomeRepoImplement` to fetch newest books from the API, converting the response into a list of `BookModel` objects.
    - Created `ServerFailure` class to handle server-related errors in the `Failure` abstract class.
    - Updated `HomeRepo` interface to change `fetchBestSellerBooks` to `fetchNewestBooks`.
    - Added Dio package dependency to `pubspec.yaml` for handling HTTP requests.
    
    This commit lays the foundation for fetching and displaying newest books in the application, enhancing the backend capabilities with robust error handling and API integration.

---
+++ lib/features/home/data/repo/home_repo_implement.dart
@@ -0,0 +1,32 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplement implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImplement(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
