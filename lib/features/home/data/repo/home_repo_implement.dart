49c31b77a085b2b5078e1698eb2ce5df63656650
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-08-11 16:32:03 +0300

    Add URL Launching Capabilities and Enhance Book Details Functionality
    This commit introduces several enhancements to the application, primarily focusing on improving the user experience when interacting with book details and enabling URL launching capabilities across multiple platforms.
    
    - **URL Launching Integration**:
      - Added support for URL launching in Android, iOS, Linux, macOS, and Windows by integrating the `url_launcher` plugin and configuring necessary platform-specific settings.
      - Created utility functions `customUrlLauncher` and `showSnackBar` to handle URL launching and display snack bar messages, respectively.

--- lib/features/home/data/repo/home_repo_implement.dart
+++ lib/features/home/data/repo/home_repo_implement.dart
@@ -1,6 +1,6 @@
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
@@ -51,18 +51,15 @@ class HomeRepoImplement implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async {
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
     try {
       var data = await apiService.get(
           endPoint:
               'volumes?Filtering=free-ebooks&q=subject:$category&Sorting=relevance');
       List<BookModel> books = [];
       for (var item in data['items']) {
-        try {
-          books.add(BookModel.fromJson(item));
-        } catch (e) {
-          books.add(BookModel.fromJson(item));
-        }
+        books.add(BookModel.fromJson(item));
       }
       return right(books);
     } catch (e) {
